aa718ef53ed6bb4c499050b17ebed8ea0f5886e9
yasmeen shalaby <yasmeenshalaby16@gmail.com>
2019-02-16 14:13:35 +0200

    admin panel6

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
 //Create http server
+// var express = require('express');
+// var multer = require("multer");
+// var server = express();
+// var mongoose = require('mongoose');
+// require('dotenv').config();
+// var expressValidator = require('express-validator');
+// server.use(expressValidator());
+// var path = require("path");
+//
+//
+// //database connection
+// const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Matgary';
+// mongoose.Promise = global.Promise;
+// mongoose.connect(CONNECTION_URI
+//   ,{ useNewUrlParser: true }
+// );
+// mongoose.set("debug",true);
+// const PORT = process.env.PORT || 8090 ;
+// server.set('views', path.join(__dirname, 'views'));
+// server.set("view engine","ejs");
+// server.set("views","./views");
+// server.use(express.static(path.join(__dirname, 'public')));
+//
+// var uploadMid = multer({dest:"./public/imgs"});
+
+
 var express = require('express');
-var multer = require("multer");
 var server = express();
-var mongoose = require('mongoose');
 require('dotenv').config();
+
+var createError = require('http-errors');
+var path = require("path");
 var expressValidator = require('express-validator');
 server.use(expressValidator());
-var path = require("path");
-//database connection
+var mongoose = require('mongoose');
+
 const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Matgary';
-mongoose.Promise = global.Promise;
+mongoose.promise = global.promise;
+
 mongoose.connect(CONNECTION_URI
   ,{ useNewUrlParser: true }
 );
-mongoose.set("debug",true);
+
 const PORT = process.env.PORT || 8090 ;
 server.set('views', path.join(__dirname, 'views'));
 server.set("view engine","ejs");
 server.set("views","./views");
 server.use(express.static(path.join(__dirname, 'public')));
 
-var uploadMid = multer({dest:"./public/imgs"});
+
 
 var AuthRouts = require('./controller/authClient');
 server.use('/authClient',AuthRouts);
